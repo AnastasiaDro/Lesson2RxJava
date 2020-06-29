d9e81c74f349b9bde60a0cff738a65ce205217ad
AnastasiaDro <adrogunova@gmail.com>
2020-06-29 23:00:26 +0300

    -Сделала класс AsyncTask внутри FirstActivity, повесила обработчик на кнопку

--- app/src/main/java/com/trainings/lesson2rxjava/first/FirstActivity.java
+++ app/src/main/java/com/trainings/lesson2rxjava/first/FirstActivity.java
@@ -2,15 +2,50 @@ package com.trainings.lesson2rxjava.first;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 import com.trainings.lesson2rxjava.R;
 
+import java.util.concurrent.TimeUnit;
+
 public class FirstActivity extends AppCompatActivity {
+
+private static final String ASYNC_TAG = "AsyncTask";
+private static final String ONCLICK_TAG = "ButtonClick метод";
+
+private MyAsynkTask myAsynkTask;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
     }
+
+    public void onTaskBtnClick(View view) {
+        myAsynkTask = new MyAsynkTask();
+        myAsynkTask.execute();
+        Log.d(ONCLICK_TAG, "Метод завершен, нажатие на кнопку обработано " + Thread.currentThread().getName());
+    }
+
+
+    private static class MyAsynkTask extends AsyncTask <Void, Void,Void> {
+        @Override
+        protected Void doInBackground(Void... voids) {
+            try {
+                for (int i = 0; i < 5; i++){
+                    TimeUnit.SECONDS.sleep(1);
+                    Log.d(ASYNC_TAG, "Это сообщение N" + i + "имя потока: " +Thread.currentThread().getName());
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+    }
+
+
+
 }
