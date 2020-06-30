1bf14b0ba3bb0fbf05fd74608a0b21b7330b32a7
AnastasiaDro <adrogunova@gmail.com>
2020-06-30 22:25:37 +0300

    -сделала: презентер и счётчик По нажатию на "start count" запускается бесконечный цикл с отправкой сообщений, при нажатии на "stop count" - счёт останавливается

--- app/src/main/java/com/trainings/lesson2rxjava/third/ThirdActivity.java
+++ app/src/main/java/com/trainings/lesson2rxjava/third/ThirdActivity.java
@@ -3,14 +3,67 @@ package com.trainings.lesson2rxjava.third;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 import com.trainings.lesson2rxjava.R;
 
+import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+
 public class ThirdActivity extends AppCompatActivity {
+
+    private static final String TAG = "ThirdActivity";
+
+    private ThirdPresenter thirdP;
+    private Observable <String> observable;
+    private Disposable disposable;
+    private TextView counterTV;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_third);
+        counterTV = findViewById(R.id.counterTV);
+        thirdP = new ThirdPresenter();
+      //  observable = thirdP.
     }
+
+    public void onStartCountClick(View view) {
+        observable.observeOn(AndroidSchedulers.mainThread()).subscribe(new Observer<String>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+                Log.d(TAG, "onSubscribe");
+                ThirdActivity.this.disposable = d;
+            }
+
+            @Override
+            public void onNext(String s) {
+                Log.d(TAG, "onNext: " + Thread.currentThread().getName() + ": " + s);
+                counterTV.setText(s);
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.e(TAG, "onError: " + e);
+            }
+
+            @Override
+            public void onComplete() {
+                Log.d(TAG, "onComplete: ");
+            }
+        });
+        Log.d(TAG, "Метод onStartCountClick закончился в потоке "+Thread.currentThread().getName());
+    }
+
+
+    public void onStopCountClick(View view) {
+        disposable.dispose();
+    }
+
+
 }
--- app/src/main/java/com/trainings/lesson2rxjava/third/ThirdPresenter.java
+++ app/src/main/java/com/trainings/lesson2rxjava/third/ThirdPresenter.java
@@ -1,4 +1,42 @@
 package com.trainings.lesson2rxjava.third;
 
+
+
+import android.util.Log;
+
+import java.util.concurrent.TimeUnit;
+
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.Scheduler;
+import io.reactivex.schedulers.Schedulers;
+
+
 public class ThirdPresenter {
+    private static final String TAG = "ThirdPresenter";
+
+    public Observable <String> startCount(){
+        Observable<String> observable = Observable.create(new ObservableOnSubscribe<String>() {
+            @Override
+            public void subscribe(ObservableEmitter<String> emitter) throws Exception {
+                try {
+                    while (true) {
+                        int i = 0;
+                        TimeUnit.SECONDS.sleep(1);
+                        String counterStr = "Счёт: " + i;
+                        i++;
+                        Log.d(TAG, "startCount поток " + Thread.currentThread().getName() + ": " + counterStr);
+                        emitter.onNext(counterStr);
+                    }
+
+                   // emitter.onComplete();
+                } catch (InterruptedException e) {
+                    Log.d(TAG, "Не удалось подсчитать");
+                }
+            }
+        }).subscribeOn(Schedulers.io());
+        return observable;
+    }
+
 }
